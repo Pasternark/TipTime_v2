101ceed92fdfd6b21639856216f42640690bcd4b
Pasternark <38192729@uaz.edu.mx>
2022-03-06 21:55:42 -0600

    version con temas claro/oscuro y estilos a cajas de texto, grupo de botones y switch

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,28 +1,37 @@
 package com.example.tiptime
 
+//Imports de la clase
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {  //Inicio de la clase MainActivity
+    //Declaracion de variables
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {    //Inicio del metodo onCreate
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateButton.setOnClickListener { calculateTip() }
-    }
+        binding.calculateButton.setOnClickListener { calculateTip() }   //Llamada al listener para ocultar el teclado
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+    }   //Fin del metodo onCreate
 
+        //Metodo calculateTip que nos permite calcular la propina segun un porcentaje marcado
         private fun calculateTip(){
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null || cost == 0.0){
             displayTip(0.0)
             return
         }
+
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
@@ -38,9 +47,22 @@ class MainActivity : AppCompatActivity() {
         displayTip(tip)
     }
 
+    //displayTip permite mostrar el resultado de calcular la propina
     private fun displayTip(tip: Double){
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    //Metodo listener, si se pulsa la tecla ENTER en el teclado liviano
+    //El teclado se oculta
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
 }
